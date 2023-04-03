9aca5888a7b19e87f64e4b5ad031c9d59cb45f15
lili <hermanl@stud.uni-obuda.hu>
2023-04-03 18:51:40 +0200

    HAZI05 edited

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -24,41 +24,51 @@ class KNNClassifier:
         x,y = dataset.iloc[:,:-1],dataset.iloc[:,-1]
         return x,y
     
-    def train_test_slpit(self, features: pd.DataFrame,
-                        labels: pd.Series) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    def train_test_split(self, features: pd.DataFrame,
+                     labels: pd.DataFrame) -> None:
         test_size = int(len(features) * self.test_split_ratio)
         train_size = len(features) - test_size
         assert len(features) == test_size + train_size, "Size mismatch!"
-        self.x_train,self.y_train = features.iloc[:train_size,:], labels.iloc[:train_size]
-        self.x_test,self.y_test = features.iloc[train_size:,:],labels.iloc[train_size:]
-        
-    def euclidean(self, element_of_x:np.ndarray) -> np.ndarray:
-        return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis=1))
-    
-    def predict(self) -> pd.Series:
-        labels_pred = []
-        for x_test_element in self.x_test.iterrows():
-            distances = self.euclidean(x_test_element)
-            distances = pd.concat([distances, self.y_train], axis=1)
-            distances = distances.sort_values(by=0).values
 
-            label_pred = mode(distances[:self.k,1], axis=0).mode[0]
+        x_train, y_train = features.iloc[:train_size, :].reset_index(drop=True), labels.iloc[:train_size].reset_index(drop=True)
+        x_test, y_test = features.iloc[train_size:, :].reset_index(drop=True), labels.iloc[train_size:].reset_index(drop=True)
+        
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
+        
+    def euclidean(self, element_of_x: pd.Series) -> pd.DataFrame:
+        return pd.np.sqrt(((self.x_train - element_of_x)**2).sum(axis=1))
+    
+    def predict(self, x_test: pd.DataFrame) -> pd.DataFrame:
+        labels_pred =[]
+        for i, x_test_element in x_test.iterrows():
+
+            distances = self.euclidean(x_test_element)
+            distances = pd.DataFrame({'distances': distances, 'labels': self.y_train})
+            distances.sort_values(by='distances', inplace=True)
+
+            label_pred = mode(distances.iloc[:self.k, 1], keepdims = False).mode
+
             labels_pred.append(label_pred)
-        self.y_preds = pd.Series(labels_pred, dtype=np.int64)
+        self.y_preds = pd.Series(labels_pred)
     
     def accuracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()
         return true_positive / len(self.y_test) * 100
-
-    def confusion_matrix(self) -> pd.DataFrame:
-        return pd.crosstab(self.y_test, self.y_preds)
-
-    def best_k(self) -> Tuple[int, float]:
-        accuracies = []
-        for k in range(1,21):
-            self.k = k
-            self.predict()
-            accuracy = self.accuracy()
-            accuracies.append((k,accuracy))
-        best_k = max(accuracies, key=lambda x:x[1])
-        return (best_k[0], round(best_k[1], 2))
+    
+    def confusion_matrix(self) -> np.ndarray:
+        return confusion_matrix(self.y_test, self.y_preds)
+    
+    @staticmethod
+    def best_k(x: pd.DataFrame, y: pd.Series) -> Tuple[int, float]:
+        best = (0, 0)
+        for i in range(1, 21):
+            classifier = KNNClassifier(i, 0.2)
+            classifier.train_test_split(x, y)
+            classifier.predict(classifier.x_test)
+            accuracy = classifier.accuracy()
+            if best[1] < accuracy:
+                best = (i, round(accuracy, 2))
+        return best
